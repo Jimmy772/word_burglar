f11ae4818ac3411b6058e0618b9f582bed17601f
Jimmy772 <bobcook772@outlook.com>
2023-11-27 20:32:56 +0300

    added filter by rating

--- chat/actions/actions.py
+++ chat/actions/actions.py
@@ -19,48 +19,56 @@ load_dotenv('../.env')
 
 
 class ActionQueryDatabase(Action):
-
     def name(self) -> Text:
         return "action_query_database"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Extract ratings entity
+        ratings_entity = next(tracker.get_latest_entity_values("rating"), None)
 
-        # Extract relevant information from user input
-        user_input = tracker.latest_message.get('text')
-        
-        # Establish a connection to the database
-        connection = psycopg2.connect(
-            user=os.getenv('DB_USERNAME'),
-            password=os.getenv('DB_PASSWORD'),
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
-            database=os.getenv('DB_NAME')
-        )
-        
-        # Create a cursor to execute SQL queries
-        cursor = connection.cursor()
-        
-        # Example SQL query
-        q = f"SELECT * FROM books WHERE average_rating = {float(user_input)} LIMIT 5"
-        
-        # Execute the query
-        cursor.execute(q)
-        
-        # Fetch the results
-        results = cursor.fetchall()
-        
-        # Process results and construct a response
-        response = "Here are the results:\n"
-        for row in results:
-            response += str(row) + "\n"
+        # Check if ratings_entity is present and convert it to a float
+        if ratings_entity:
+            try:
+                rating_value = float(ratings_entity)
+            except ValueError:
+                dispatcher.utter_message("Invalid rating value provided.")
+                return []
 
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
+            # Establish a connection to the database
+            connection = psycopg2.connect(
+                user=os.getenv('DB_USERNAME'),
+                password=os.getenv('DB_PASSWORD'),
+                host=os.getenv('DB_HOST'),
+                port=os.getenv('DB_PORT'),
+                database=os.getenv('DB_NAME')
+            )
 
-        # Send the response back to the user
-        dispatcher.utter_message(response)
+            # Create a cursor to execute SQL queries
+            cursor = connection.cursor()
+
+            # Example SQL query with proper parameterization to avoid SQL injection
+            q = "SELECT title FROM books WHERE average_rating = %s LIMIT 5"
+
+            # Execute the query with the rating_value as a parameter
+            cursor.execute(q, (rating_value,))
+
+            # Fetch the results
+            results = cursor.fetchall()
+
+            # Process results and construct a response
+            response = "Here are the results:\n"
+            for row in results:
+                response += str(row) + "\n"
+
+            # Close the cursor and connection
+            cursor.close()
+            connection.close()
+
+            # Send the response back to the user
+            dispatcher.utter_message(response)
+        else:
+            dispatcher.utter_message("No rating entity found in your input.")
 
         return []
